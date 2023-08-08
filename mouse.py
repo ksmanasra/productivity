50eba70963d50758b76011c6259c20371c7a782d
ksmanasra <ksmanasra@gmail.com>
2023-08-08 15:10:09 -0500

    try1

---
+++ mouse.py
@@ -0,0 +1,33 @@
+import pyautogui
+import time
+
+def move_mouse():
+    count = 0
+    while True:
+        try: 
+            x, y = pyautogui.position()
+
+            pyautogui.moveTo(x + 10, y + 10)
+
+            time.sleep(10)
+
+            x, y = pyautogui.position()
+
+            pyautogui.moveTo(x - 10, y - 10)
+
+            time.sleep(10)
+
+            if count == 500:
+                pyautogui.keyDown('ctrl')
+                pyautogui.keyDown('command')
+                pyautogui.keyDown('q')
+                break
+            else: 
+                count += 1
+        except KeyboardInterrupt:
+            print("\nScript terminated by user.")
+            break
+
+if __name__ == "__main__":
+    print("Press Ctrl + C to stop the script.")
+    move_mouse()
